549ce9bfb9e997e5dc03d34f9309a99b8f9db882
Pravallika2003HUB <pravalikapothanapalli@gmail.com>
2026-09-24 16:54:38 +0530

    Configure MySQL SSL for deployment

--- smart_housing/settings.py
+++ smart_housing/settings.py
@@ -64,8 +64,11 @@ DATABASES = {
         "PASSWORD": os.getenv("DB_PASSWORD", ""),
         "HOST": os.getenv("DB_HOST", "127.0.0.1"),
         "PORT": os.getenv("DB_PORT", "3306"),
-        "OPTIONS": {"charset": "utf8mb4"},
-    }
+        "OPTIONS": {
+    "charset": "utf8mb4",
+    "ssl": {"ssl-mode": "REQUIRED"},
+},
+    
 }
 
 AUTH_USER_MODEL = "accounts.User"
